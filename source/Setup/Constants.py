50c27ca467b0e38859335a7f80c28f6fade190c3
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-19 16:30:40 +0000

    Created first level and set up env for Brick implementation

--- source/Setup/Constants.py
+++ source/Setup/Constants.py
@@ -1,7 +1,9 @@
-SCREEN_SIZE: tuple[int, int] = (1200, 800)
 
+# Environment & Physics #
+SCREEN_SIZE: tuple[int, int] = (1200, 800)
 FPS: int = 60
 
+# Paddle #
 PADDLE_OFFSET: int = 50
 PADDLE_SIZE: tuple[int, int] = (150, 20)
 INITIAL_PADDLE_POSITION: tuple[int, int] = (
@@ -10,5 +12,16 @@ INITIAL_PADDLE_POSITION: tuple[int, int] = (
 )
 PADDLE_BORDER_RAD: int = 10
 
+# Ball #
 DEFAULT_BALL_RAD: int = 10
 BALL_SPEED: int = 10
+
+# Bricks #
+NUM_OF_BRICKS: tuple[int, int] = (32, 30)
+BRICK_SIZE: tuple[float, float] = (SCREEN_SIZE[0]/NUM_OF_BRICKS[0],
+                                   (SCREEN_SIZE[1]*(3/4))/NUM_OF_BRICKS[1])
+
+# Files #
+_RESOURCES_FP: str = "../Resources/"
+
+LEVELS_FP: str = f"{_RESOURCES_FP}Levels/"
