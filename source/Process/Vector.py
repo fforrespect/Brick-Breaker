a3fc4bf8dcf62e39a66cd30cd6fd61a0269e2612
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-19 00:17:30 +0000

    Added type hints to Vector.py and adjusted it for use in this project

--- source/Process/Vector.py
+++ source/Process/Vector.py
@@ -2,33 +2,28 @@ from math import sqrt
 
 
 # Some maths to work out the unit vector between two positions
-def unit_vector(pos1, pos2):
+def unit_vector(pos1: tuple[float, float] | list[int], pos2: tuple[float, float] | list[int]) -> tuple[float, float]:
+    pos1, pos2 = tuple[float, float](pos1), tuple[float, float](pos2)
     # Find the vector itself
-    vector = _vector_from_pos(pos1, pos2)
+    vector: tuple[float, float] = __vector_from_pos(pos1, pos2)
     # Then find the magnitude of that vector
-    vector_mag = magnitude(vector)
+    vector_mag: float = __magnitude(vector)
     # Then divide each part of the vector by the magnitude, and return it
-    #   (as a float)
-    return list(map(lambda x: x/vector_mag, vector))
+    #   (as a float tuple)
+    return tuple[float, float](map(lambda x: x/vector_mag, vector))
 
 
-def distance(pos1, pos2):
+def distance(pos1: tuple[float, float], pos2: tuple[float, float]) -> float:
     # First find the vector itself
-    vector = _vector_from_pos(pos1, pos2)
-    # Then find the euclidean distance between the two points
-    return magnitude(vector)
+    vector: tuple[float, float] = __vector_from_pos(pos1, pos2)
+    # Then find the Euclidean distance between the two points
+    return __magnitude(vector)
 
 
-# l_or_r = 1 if right, -1 if left
-def distance_l_r(moving_point, stationary_point, l_or_r):
-    x = moving_point[0] + l_or_r, moving_point[1]
-    return distance(x, stationary_point)
-
-
-def magnitude(vector):
-    # Find the euclidean distance between the two points
+def __magnitude(vector: tuple[float, float]) -> float:
+    # Find the Euclidean distance between the two points
     return sqrt(vector[0]**2 + vector[1]**2)
 
 
-def _vector_from_pos(pos1, pos2):
+def __vector_from_pos(pos1: tuple[float, float], pos2: tuple[float, float]) -> tuple[float, float]:
     return pos1[0]-pos2[0], pos1[1]-pos2[1]
