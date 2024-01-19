0206f2739fda6f1fcc2bb952c31ba576e3fab877
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-19 17:08:50 +0000

    Added colours for varying strengths

--- source/Setup/Colours.py
+++ source/Setup/Colours.py
@@ -15,3 +15,23 @@ SILVER : tuple[int, int, int] = (192, 192, 192)
 TEAL   : tuple[int, int, int] = (  0, 128, 128)
 WHITE  : tuple[int, int, int] = (255, 255, 255)
 YELLOW : tuple[int, int, int] = (255, 255,   0)
+
+one     : tuple[int, int, int] = (  6,   1, 245)
+two     : tuple[int, int, int] = ( 55, 126,  34)
+three   : tuple[int, int, int] = (234,  51,  35)
+four    : tuple[int, int, int] = (  1,   0, 123)
+five    : tuple[int, int, int] = (115,  20,  12)
+six     : tuple[int, int, int] = ( 54, 124, 125)
+seven   : tuple[int, int, int] = (  0,   0,   0)
+eight   : tuple[int, int, int] = ( 86,  84,  87)
+
+strength: dict[int, tuple[int, int, int]] = {
+    1: one,
+    2: two,
+    3: three,
+    4: four,
+    5: five,
+    6: six,
+    7: seven,
+    8: eight
+}
