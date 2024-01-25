27afd36d2bef0df952f2dad207c5d68c66575eb1
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-25 12:24:56 +0000

    Started process of creating collision triangles

--- source/Process/Triangle.py
+++ source/Process/Triangle.py
@@ -1,9 +1,13 @@
+from typing import Literal
+
+from Setup import Constants as c
+
 
 class Instance:
     def __init__(self, points: tuple[tuple[float, float], tuple[float, float], tuple[float, float]]):
         self.points: tuple[tuple[float, float], tuple[float, float], tuple[float, float]] = points
 
-    # Created with GPT-4 #
+    # Method created with GPT-4 #
     def point_inside(self, point: tuple[float, float]) -> bool:
         """
         Check if a given point is inside the triangle.
@@ -22,3 +26,22 @@ class Instance:
         b3: bool = sign(point, self.points[2], self.points[0]) < 0.0
 
         return (b1 == b2) and (b2 == b3)
+
+
+def create_collision_triangle(
+        left_point: tuple[float, float],
+        right_point: tuple[float, float],
+        direction: Literal['top', 'bottom', 'left', 'right']) -> Instance:
+
+    x_l, y_lr = left_point
+    x_r = right_point[0]
+
+    height = c.DEFAULT_BALL_RADIUS*2
+
+    p1 = (x_l - height, y_lr - height)
+    p2 = (x_r + height, y_lr - height)
+
+    midpoint = (x_r + x_l)/2
+    p3 = (midpoint, y_lr - midpoint)
+
+    return Instance((p1, p2, p3))
