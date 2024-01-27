5db9bf8342d1cbd45304a5b32a083df2193d0889
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-27 22:08:49 +0000

    Commented Level.py

--- source/Process/Level.py
+++ source/Process/Level.py
@@ -8,19 +8,27 @@ def _decode(level: int) -> tuple[str, ...]:
     with open(f"{c.LEVELS_FP}{level}.txt", 'r') as file:
         lines: list[str] = file.read().split("\n")
 
+    # remove the first line - it will always be irrelevant
     lines = lines[1:]
+
+    # -- Format each of the lines correctly -- #
+    # line[1:]                        : remove the first char in every line - it will also always be irrelevant
+    # .replace(".", " ")              : replace all '.'s with spaces, since they are only in the level files as a guide
+    # .ljust(c.NUM_OF_BRICKS[0], " ") : make every line the same length
     lines = list(map(
         lambda line:
             line[1:].replace(".", " ").ljust(c.NUM_OF_BRICKS[0], " "),
         lines)
     )
 
+    # remove any trailing empty lines, for efficiency
     for line_i in reversed(range(len(lines))):
         if lines[line_i] == " "*c.NUM_OF_BRICKS[0]:
             del lines[line_i]
         else:
             break
 
+    # add the new level to the _levels dictionary
     _levels[level] = tuple(lines)
 
     return tuple(lines)
